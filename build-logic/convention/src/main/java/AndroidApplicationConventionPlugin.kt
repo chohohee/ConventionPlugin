271dcbd9828ffeadc035e971fdb06f38158f2368
chohohee <gone1005@gmail.com>
2024-10-13 15:09:27 +0900

    Add android application plugin

---
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,16 @@
+import com.chh.convention.configureKotlinAndroid
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+internal class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+
+            configureKotlinAndroid()
+        }
+    }
+}
