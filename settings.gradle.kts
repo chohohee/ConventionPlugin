cd3cbd4112752a803145f6cc35e2133aa6084c42
chohohee <gone1005@gmail.com>
2024-10-13 15:05:04 +0900

    Add convention plugins module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,13 @@
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -21,4 +18,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "ConventionPlugin"
 include(":app")
- 
